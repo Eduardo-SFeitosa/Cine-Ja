324f65641acd76ebed54b0d59b082167791ba9c6
Eduardo <eduardo.sfeitosa03@gmail.com>
2026-06-01 09:13:07 -0300

    adicionada novas mudancas no server para criar itinerario caso esteja vazio

--- server/server.js
+++ server/server.js
@@ -88,7 +88,23 @@ async function iniciarServidorComBanco(tentativa = 1) {
 
 async function checar_itinerario() {
 
-  const sessoes_disponiveis = await sessoes_services.sessoes_existentes()
+  const filmes_disponiveis = await sessoes_services.filmes_em_cartaz()
+
+  if (filmes_disponiveis.length <= 0){
+
+    await itinerario_services.criar_itinerario()
+
+    console.log("itinerario criado")
+
+    return true
+
+  }
+
+  const filme_id = filmes_disponiveis[0]["filme_rel.id"]
+  
+  const data_hoje = new Date().toISOString().slice(0, 10)
+  
+  const sessoes_disponiveis = await sessoes_services.sessoes_por_filme_id(filme_id, data_hoje)
 
   if (sessoes_disponiveis.length <= 0) {
 
